431e148bfa7007394e55e678016db4352669a34b
R-Dhanush <dhanu72004@gmail.com>
2024-08-16 23:35:31 +0530

    contact & js changes

--- script.js
+++ script.js
@@ -7,16 +7,28 @@ function closesidenav() {
   sidenav.style.right = "-100%";
 }
 
-window.addEventListener("scroll", function () {
-  var nav_bar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector('.navbar');
+  const homeSection = document.querySelector('#home');
+  const para1 = document.querySelector('#para1');
+  const para2 = document.querySelector('#para2');
 
-  if (window.scrollY > 2200) {
-    nav_bar.style.backgroundColor = "#7d7d7d";
-  } else if (window.scrollY > 1100) {
-    nav_bar.style.backgroundColor = "#5d5d5d";
-  } else if (window.scrollY > 50) {
-    nav_bar.style.backgroundColor = "#3d3d3d";
-  } else {
-    nav_bar.style.backgroundColor = "#1d1d1d";
-  }
+  const changeNavbarColor = () => {
+    const scrollPosition = window.scrollY;
+    const para1Offset = para1.offsetTop;
+    const para2Offset = para2.offsetTop;
+
+    if (scrollPosition >= para2Offset) {
+      navbar.style.backgroundColor = "#7d7d7d";
+    } else if (scrollPosition >= para1Offset) {
+      navbar.style.backgroundColor = "#5d5d5d";
+    } else if (window.scrollY > 50) {
+      navbar.style.backgroundColor = "#3d3d3d";
+    } else {
+      navbar.style.backgroundColor = "#1d1d1d";
+    }
+  };
+  window.addEventListener('scroll', changeNavbarColor);
 });
+
+
